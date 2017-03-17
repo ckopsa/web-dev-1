c17d223921067ef09830f25cf138ee37f2d70db5
coljamkop <coljamkop@gmail.com>
2017-03-17 17:53:19 -0600

    problem 1 done

--- week11/problem1.php
+++ week11/problem1.php
@@ -1,1 +1,10 @@
-<?php echo '<p>Hello World</p>'; ?>
+<?php
+    echo "<html>";
+echo "<head>";
+echo "<title>Week 11 : Problem 1</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>Hello World</p>";
+echo "</body>";
+echo "</html>";
+?>
