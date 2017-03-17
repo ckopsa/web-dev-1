64554f737067688150627618c25cf11dd91afb69
coljamkop <coljamkop@gmail.com>
2017-03-17 17:59:09 -0600

    Forgot to add time :)

--- week11/problem2.php
+++ week11/problem2.php
@@ -4,7 +4,7 @@ echo "<head>";
 echo "<title>Week 11 : Problem 2</title>";
 echo "</head>";
 echo "<body>";
-echo "<p>" . date("d/m/Y") . "</p>";
+echo "<p>Date: " . date("d/m/Y") . "Time: " . date("h:i:sa") . "</p>";
 echo "</body>";
 echo "</html>";
 ?>
