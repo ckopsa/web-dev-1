d96365b87245aae349bc15ad4d9f0937a591c407
coljamkop <coljamkop@gmail.com>
2017-03-18 16:04:20 -0600

    problem 3

---
+++ week11/problem3.php
@@ -0,0 +1,12 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<title>Week 11 : Problem 2</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>";
+echo POST["input3"];
+echo "</p>";
+echo "</body>";
+echo "</html>";
+?>
