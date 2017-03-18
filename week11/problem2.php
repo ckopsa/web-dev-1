3f79540361e6c5b52713030b4116f3924b7f493a
coljamkop <coljamkop@gmail.com>
2017-03-17 18:21:54 -0600

    come on baby...

--- week11/problem2.php
+++ week11/problem2.php
@@ -4,7 +4,7 @@ echo "<head>";
 echo "<title>Week 11 : Problem 2</title>";
 echo "</head>";
 echo "<body>";
-echo "<p>Today is " . date("l"). ", the " . date("j") . date("S") . " of " . date("F"). " in the year of our Lord " . date("Y") . date("h:i:sa") . "</p>";
+echo "<p>Today is " . date("l"). ", the " . date("j") . date("S") . " of " . date("F"). " in the year of our Lord " . date("Y") . ", the time being " . date("h:i:sa") . "</p>";
 echo "</body>";
 echo "</html>";
 ?>
