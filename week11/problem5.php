4e66499a6879e4c74e6586192c99a26c085319e2
coljamkop <coljamkop@gmail.com>
2017-03-18 16:51:18 -0600

    round 1

---
+++ week11/problem5.php
@@ -0,0 +1,14 @@
+<?php
+echo "<html>";
+echo "<head>";
+echo "<title>Week 11 : Problem 5</title>";
+echo "</head>";
+echo "<body>";
+echo "<p>";
+for ($x = 1; $x <= $_POST["n"]; $x++) {
+    echo "The number is: $x <br>";
+} 
+echo "</p>";
+echo "</body>";
+echo "</html>";
+?>
