449b4db5d25cf56942dd68c4da9f11bc24cdf3f5
coljamkop <coljamkop@gmail.com>
2017-03-17 18:07:44 -0600

    please work...

--- week11/problem2.php
+++ week11/problem2.php
@@ -4,7 +4,7 @@ echo "<head>";
 echo "<title>Week 11 : Problem 2</title>";
 echo "</head>";
 echo "<body>";
-echo "<p>Date: " . date("d/m/Y") . " Time: " . date("h:i:sa") . "</p>";
+echo "<p>Today is " . date("l"). ", the " . date("D") . " of " . date("F"). " in the year of our Lord " . date("Y") . date("h:i:sa") . "</p>";
 echo "</body>";
 echo "</html>";
 ?>
