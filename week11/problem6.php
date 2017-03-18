c09f904d3e949d6a2a9ddae5ba2862a4db5b77d7
coljamkop <coljamkop@gmail.com>
2017-03-18 17:57:30 -0600

    fuh

--- week11/problem6.php
+++ week11/problem6.php
@@ -5,9 +5,10 @@ echo "<title>Week 11 : Problem 6</title>";
 echo "</head>";
 echo "<body>";
 echo "<p>test stuff.. :)</p>";
-echo "<table>";
 echo $_POST["input6"];
 $sortedStrings = sort(explode(", ", $_POST["input6"]), SORT_STRING);
+echo $sortedStrings;
+echo "<table>";
 foreach ($sortedStrings as $string) {
     echo "<tr>";
     echo $string;
