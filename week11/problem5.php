4f2020b048c9453f48057a8e45ce3247c8abbd2e
coljamkop <coljamkop@gmail.com>
2017-03-18 17:03:39 -0600

    round 333333

--- week11/problem5.php
+++ week11/problem5.php
@@ -4,11 +4,19 @@ echo "<head>";
 echo "<title>Week 11 : Problem 5</title>";
 echo "</head>";
 echo "<body>";
-echo "<p>";
+echo "<table>";
+echo "<tr>";
+echo "<th>N</th>";
+echo "<th>X^n</th> ";
+echo "</tr>";
 for ($x = 1; $x <= $_POST["n"]; $x++) {
+    echo "<tr>";
+    echo "<td>$x</td>";
+    echo "<td>0</td> ";
+    echo "</tr>";
     echo "The number is: $x <br>";
-} 
-echo "</p>";
+}
+echo "</table>";
 echo "</body>";
 echo "</html>";
 ?>
