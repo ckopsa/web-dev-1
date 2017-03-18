c07dc5a46f34ae467e89e5752d190ec839efd3b8
coljamkop <coljamkop@gmail.com>
2017-03-18 17:52:49 -0600

    I don't know if they do...

--- week11/problem6.php
+++ week11/problem6.php
@@ -4,6 +4,7 @@ echo "<head>";
 echo "<title>Week 11 : Problem 6</title>";
 echo "</head>";
 echo "<body>";
+echo "<p>test stuff.. :)</p>";
 echo "<table>";
 $sortedStrings = sort(explode(", ", $_POST["input6"]), SORT_STRING);
 foreach ($sortedStrings as $string) {
